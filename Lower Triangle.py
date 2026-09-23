39920cbfcbf2f391b690a881c8551703c477a1ed
kaurmaanya18-stack <kaurmaanya18@gmail.com>
2026-09-23 17:16:05 +0530

    Add centered inverted pyramid pattern

---
+++ Lower Triangle.py
@@ -0,0 +1,23 @@
+#########
+ #######
+  #####
+   ###
+    #
+for i in range (1,6):
+    for j in range(1,10):
+        if(j==5):
+            print("#",end="")
+        elif(j<5):
+            if(j>=i):
+                print("#",end="")
+            else:
+                print(" ",end="")
+        elif(j>5):
+            k=j-5
+            if((k+i)<=5):
+                print("#",end="")
+            else:
+                print(" ",end="")
+        else:
+            print(" ",end="")
+    print()

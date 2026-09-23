b598525f4bc7b0c274eac561967fc2a81e3c5945
kaurmaanya18-stack <kaurmaanya18@gmail.com>
2026-09-23 17:00:37 +0530

    Add right aligned hash triangle pattern

---
+++ Right-Aligned Increasing Hash Triangle.py
@@ -0,0 +1,12 @@
+#     #
+#    ##
+#   ###
+#  ####
+# #####
+for i in range (5,0,-1):
+    for j in range(6):
+        if(j>=i):
+            print("#",end="")
+        else:
+            print(" ",end="")
+    print()

d2e536e4b19641ff5c6cb525c9a135b158af474f
kaurmaanya18-stack <kaurmaanya18@gmail.com>
2026-09-23 16:57:39 +0530

    Add combined number triangle pattern

---
+++ Combined Increasing & Decreasing Number Triangles.py
@@ -0,0 +1,22 @@
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+# 1 2 3 4 5
+# 1 2 3 4 5
+#   2 3 4 5
+#     3 4 5
+#       4 5
+#         5
+for i in range(1,6):
+    for j in range(1,6):
+        if(j<=i):
+            print(j,end=" ")
+    print()
+for i in range(1,6):
+    for j in range(1,6):
+        if(j>=i):
+            print(j,end=" ")
+        else:
+            print(" ",end=" ")
+    print()
